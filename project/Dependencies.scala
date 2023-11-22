50eb6fe6200cbad54ffb1c496603e630198ff969
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-22 08:32:38 +0000

    Update commons-lang3 to 3.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val commonsLang3 = "org.apache.commons" % "commons-lang3" % "3.13.0"
+  lazy val commonsLang3 = "org.apache.commons" % "commons-lang3" % "3.14.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
 }
