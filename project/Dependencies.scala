703afdf7d2d9d1c870c4d7e96de506740181b943
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-05-29 14:57:33 +0100

    update metadataSchema version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
   lazy val ujson = "com.lihaoyi" % "ujson_native0.5_2.13" % "3.3.1"
   lazy val jacksonModule = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.17.0"
-  lazy val metadataSchema = "uk.gov.nationalarchives" % "da-metadata-schema_3" % "0.0.20"
+  lazy val metadataSchema = "uk.gov.nationalarchives" % "da-metadata-schema_3" % "0.0.21"
 }
