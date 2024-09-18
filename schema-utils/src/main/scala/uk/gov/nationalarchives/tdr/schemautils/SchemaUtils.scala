d2f7df1748449e4ca264e1b994fdf41450f9b2e8
TomJKing <tom.king@nationalarchives.gov.uk>
2024-09-18 11:07:43 +0100

    Original to Alternate property key mapping method

--- schema-utils/src/main/scala/uk/gov/nationalarchives/tdr/schemautils/SchemaUtils.scala
+++ schema-utils/src/main/scala/uk/gov/nationalarchives/tdr/schemautils/SchemaUtils.scala
@@ -8,12 +8,43 @@ object SchemaUtils {
   val BASE_SCHEMA = "/metadata-schema/baseSchema.schema.json"
 
   private lazy val schemaNode: JsonNode = new ObjectMapper().readTree(getClass.getResourceAsStream(BASE_SCHEMA))
+  private lazy val schemaProperties = schemaNode
+    .at("/properties")
+    .properties()
+    .asScala
+
+  /** Provides a property mapping between an original property name and an alternate property name using the base schema:
+    * https://github.com/nationalarchives/da-metadata-schema/blob/main/metadata-schema/baseSchema.schema.json
+    *
+    * @param originalKey
+    *   The original key that needs to be mapped to the alternate key
+    * @param alternateKey
+    *   The alternate key that for the original key.
+    * @return
+    *   Map with the 'originalKey' as the key and the 'alternateKey' as the value. If no value found for the 'originalKey' or 'alternateKey' then defaults to the property name
+    *   defined in the base schema
+    */
+
+  def originalKeyToAlternateKeyMapping(originalKey: String, alternateKey: String): Map[String, String] = {
+    val altKeys = schemaProperties
+      .map(p => p.getKey -> p.getValue.at("/alternateKeys").asScala)
+      .toMap
+
+    altKeys
+      .map(altKey => {
+        val defaultKeyValue = altKey._1
+        altKey._2.map(jn => {
+          val original = jn.at(s"/$originalKey")
+          val required = jn.at(s"/$alternateKey")
+          original.asText(defaultKeyValue) -> required.asText(defaultKeyValue)
+        })
+      })
+      .flatten
+      .toMap
+  }
 
   def convertToValidationKey(alternateKeyName: String, alternateKeyValue: String): String = {
-    schemaNode
-      .at("/properties")
-      .properties()
-      .asScala
+    schemaProperties
       .find(property => property.getValue.at("/alternateKeys").asScala.exists(p => p.at(s"/$alternateKeyName").asText() == alternateKeyValue))
       .map(_.getKey)
       .getOrElse("")
--- schema-utils/src/test/scala/uk/gov/nationalarchives/tdr/schemautils/SchemaUtilsTest.scala
+++ schema-utils/src/test/scala/uk/gov/nationalarchives/tdr/schemautils/SchemaUtilsTest.scala
@@ -2,9 +2,34 @@ package uk.gov.nationalarchives.tdr.schemautils
 
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
-import SchemaUtils.{convertToAlternateKey, convertToValidationKey}
+import SchemaUtils.{convertToAlternateKey, convertToValidationKey, originalKeyToAlternateKeyMapping}
 
 class SchemaUtilsTest extends AnyWordSpec {
+
+  "originalKeyToAlternateKeyMapping" should {
+    "return the correct mapping for the given original and alternate key parameters" in {
+      val res = originalKeyToAlternateKeyMapping("tdrFileHeader", "tdrDataLoadHeader")
+      res.size should be(20)
+      res("Filepath") should be("ClientSideOriginalFilepath")
+      res("Date last modified") should be("ClientSideFileLastModifiedDate")
+      res("Filename") should be("Filename")
+      res("Alternative description") should be("DescriptionAlternate")
+      res("Is the description sensitive for the public?") should be("DescriptionClosed")
+      res("FOI decision asserted") should be("FoiExemptionAsserted")
+      res("FOI exemption code") should be("FoiExemptionCode")
+      res("Closure status") should be("ClosureType")
+      res("Closure Period") should be("ClosurePeriod")
+      res("Closure Start Date") should be("ClosureStartDate")
+      res("Is the title sensitive for the public?") should be("ClosureType")
+      res("Add alternative title without the file extension") should be("TitleAlternate")
+    }
+
+    "return base schema property names where original and alternate key parameters do not exist" in {
+      val res = originalKeyToAlternateKeyMapping("unknownOriginalKey", "unknownAlternateKey")
+      res.size should be(20)
+      res.foreach(e => e._1 should equal(e._2))
+    }
+  }
 
   "convertToValidationKey" should {
 
@@ -36,6 +61,12 @@ class SchemaUtilsTest extends AnyWordSpec {
     "return empty string if the property key is valid but the alternate key name is not valid" in {
       convertToValidationKey("dddd", "date_last_modified") should be("")
     }
+  }
+
+  private val expectedKnownKeyMapping = {
+    Map(
+      "client_side_checksum" -> "SHA256ClientSideChecksum"
+    )
   }
 
 }
