3a085de0463cd98ef355384930eb2612c1ec8de6
ian-hoyle <ian.hoyle.tna@gmail.com>
2024-10-23 16:19:04 +0100

    Tdrd 62 validate columns required (#180)
    * required schema
    
    * Update MetadataValidationJsonSchemaSpec.scala

--- validation/src/main/scala/uk/gov/nationalarchives/tdr/validation/schema/ValidationProcess.scala
+++ validation/src/main/scala/uk/gov/nationalarchives/tdr/validation/schema/ValidationProcess.scala
@@ -2,5 +2,5 @@ package uk.gov.nationalarchives.tdr.validation.schema
 
 object ValidationProcess extends Enumeration {
   type ValidationProcess = Value
-  val SCHEMA_BASE, SCHEMA_CLOSURE_CLOSED, SCHEMA_CLOSURE_OPEN = Value
+  val SCHEMA_BASE, SCHEMA_CLOSURE_CLOSED, SCHEMA_CLOSURE_OPEN, SCHEMA_REQUIRED = Value
 }
