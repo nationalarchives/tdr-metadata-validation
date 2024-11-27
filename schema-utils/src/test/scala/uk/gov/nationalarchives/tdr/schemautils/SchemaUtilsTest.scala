75758bd44b4285917e99646d50e6bf346573cf85
TomJKing <tom.king@nationalarchives.gov.uk>
2024-11-27 10:48:32 +0000

    Additional util functions to support SharePoint

--- schema-utils/src/test/scala/uk/gov/nationalarchives/tdr/schemautils/SchemaUtilsTest.scala
+++ schema-utils/src/test/scala/uk/gov/nationalarchives/tdr/schemautils/SchemaUtilsTest.scala
@@ -2,9 +2,44 @@ package uk.gov.nationalarchives.tdr.schemautils
 
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
-import SchemaUtils.{convertToAlternateKey, convertToValidationKey}
+import SchemaUtils.{baseSchemaNode, convertToAlternateKey, convertToValidationKey, getClass, schemaProperties}
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 
 class SchemaUtilsTest extends AnyWordSpec {
+
+  "schemaProperties" should {
+    "return the properties for the base schema where no schema node provided" in {
+      val properties = schemaProperties()
+      properties.size should be(20)
+    }
+
+    "return the properties of the schema node provided" in {
+      val otherSchema = "/metadata-schema/dataLoadSharePointSchema.schema.json"
+      val otherSchemaNode: JsonNode = new ObjectMapper().readTree(getClass.getResourceAsStream(otherSchema))
+
+      val properties = schemaProperties(otherSchemaNode)
+      properties.size should be(6)
+    }
+  }
+
+  "baseSchemaNode" should {
+
+    "return the base schema property from the supplied '$ref' schema reference if property exists in base schema"  in {
+      val result = baseSchemaNode("classpath:/metadata-schema/baseSchema.schema.json#/properties/file_path")
+      result.isDefined should be(true)
+      result.get._1 should be("file_path")
+    }
+
+    "return 'None' if the supplied '$ref' schema reference does not exist in the base schema" in {
+      val result = baseSchemaNode("classpath:/metadata-schema/baseSchema.schema.json#/properties/unknown_property")
+      result.isDefined should be(false)
+    }
+
+    "return 'None' if an invalid '$ref' schema reference is supplied" in {
+      val result = baseSchemaNode("invalid-ref/")
+      result.isDefined should be(false)
+    }
+  }
 
   "convertToValidationKey" should {
 
@@ -24,7 +59,7 @@ class SchemaUtilsTest extends AnyWordSpec {
 
   "convertToAlternateKey" should {
 
-    "convert property key to an alternate key" in {
+    "convert property key to an alternate key for the base schema where no schema node provided" in {
       convertToAlternateKey("tdrFileHeader", "date_last_modified") should be("Date last modified")
       convertToAlternateKey("tdrDataLoadHeader", "file_path") should be("ClientSideOriginalFilepath")
     }
