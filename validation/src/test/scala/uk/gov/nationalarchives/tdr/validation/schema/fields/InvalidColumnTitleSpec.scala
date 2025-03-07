3eb143521222f85c45bd4ddc28952436e86aecdc
Mark Kingsbury <mark@kingsburydata.com>
2025-03-07 15:10:05 +0000

    TDRD-557: tweak a few tests after review (#261)
    * TDRD-557: tweak a few tests after review
    
    * TDRD-557: bump version of schema used (only change is tweaked msgs)
    
    * TDRD-557: zap space

--- validation/src/test/scala/uk/gov/nationalarchives/tdr/validation/schema/fields/InvalidColumnTitleSpec.scala
+++ validation/src/test/scala/uk/gov/nationalarchives/tdr/validation/schema/fields/InvalidColumnTitleSpec.scala
@@ -8,7 +8,7 @@ class InvalidColumnTitleSpec extends AnyWordSpecLike {
 
   "When validating against all schema we see" should {
 
-    "error(s) if an invalid column header is provided" in {
+    "succeeds if an invalid column header is provided" in {
       val openTestFileRow = openMetadataFileRow(invalidColumnTitle = Some("data in a column that with an invalid title"))
       validationErrors(openTestFileRow).size shouldBe 0
       val closedTestFileRow = closedMetadataFileRow(invalidColumnTitle = Some("data in a column that with an invalid title"))
