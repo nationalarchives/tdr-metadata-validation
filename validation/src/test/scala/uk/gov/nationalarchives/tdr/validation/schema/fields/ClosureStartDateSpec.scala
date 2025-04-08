4f41914db33d84b23f0b195de1cfa05139876fe0
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2025-04-08 17:23:45 +0100

    TDRD-648 - Add custom validator to validate closure_start_date against end_date or date_last_modified

--- validation/src/test/scala/uk/gov/nationalarchives/tdr/validation/schema/fields/ClosureStartDateSpec.scala
+++ validation/src/test/scala/uk/gov/nationalarchives/tdr/validation/schema/fields/ClosureStartDateSpec.scala
@@ -8,7 +8,7 @@ import uk.gov.nationalarchives.tdr.validation.schema.ValidationProcess.{SCHEMA_B
 
 class ClosureStartDateSpec extends AnyWordSpecLike {
 
-  "When validating against all schema we see " should {
+  "When validating against all schema we see" should {
 
     "success if no value is provided for an open record" in {
       val openTestFileRow = openMetadataFileRow(closureStartDate = Some(""))
@@ -18,6 +18,50 @@ class ClosureStartDateSpec extends AnyWordSpecLike {
     "success if a value in the past is provided for a closed record" in {
       val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-25"))
       validationErrors(closedTestFileRow).size shouldBe 0
+    }
+
+    "success if the closure_start_date matches to end_date" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-25"), dateOfRecord = Some("2024-12-25"))
+      validationErrors(closedTestFileRow).size shouldBe 0
+    }
+
+    "success if the closure_start_date matches to 31st Dec of the year of end_date" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-31"), dateOfRecord = Some("2024-12-25"))
+      validationErrors(closedTestFileRow).size shouldBe 0
+    }
+
+    "success if the closure_start_date matches to date_last_modified when no end_date provided" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-05-25"), dateLastModified = Some("2024-05-25"))
+      validationErrors(closedTestFileRow).size shouldBe 0
+    }
+
+    "success if the closure_start_date matches to 31st Dec of the year of date_last_modified when no end_date provided" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-31"), dateLastModified = Some("2024-05-25"))
+      validationErrors(closedTestFileRow).size shouldBe 0
+    }
+
+    "error(s) if the closure_start_date doesn't match to end_date but matches the date_last_modified" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-25"), dateOfRecord = Some("2023-02-25"), dateLastModified = Some("2024-12-25"))
+      validationErrors(closedTestFileRow).size shouldBe 1
+      validationErrors(closedTestFileRow) should contain theSameElementsAs List(
+        ValidationError(SCHEMA_BASE, "closure_start_date", "matchEndDateOrDateLastModified")
+      )
+    }
+
+    "error(s) if the closure_start_date doesn't match to end_date or its 31st Dec of the year" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-31"), dateOfRecord = Some("2023-04-25"))
+      validationErrors(closedTestFileRow).size shouldBe 1
+      validationErrors(closedTestFileRow) should contain theSameElementsAs List(
+        ValidationError(SCHEMA_BASE, "closure_start_date", "matchEndDateOrDateLastModified")
+      )
+    }
+
+    "error(s) if the closure_start_date doesn't match to date_last_modified or its 31st Dec of the year" in {
+      val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("2024-12-31"), dateLastModified = Some("2023-04-25"))
+      validationErrors(closedTestFileRow).size shouldBe 1
+      validationErrors(closedTestFileRow) should contain theSameElementsAs List(
+        ValidationError(SCHEMA_BASE, "closure_start_date", "matchEndDateOrDateLastModified")
+      )
     }
 
     "error(s) if the column is missing" in {
@@ -42,7 +86,8 @@ class ClosureStartDateSpec extends AnyWordSpecLike {
       val openTestFileRow = openMetadataFileRow(closureStartDate = Some("3024-12-25"))
       validationErrors(openTestFileRow) should contain theSameElementsAs List(
         ValidationError(SCHEMA_CLOSURE_OPEN, "closure_start_date", "type"), // Must be empty for an open record
-        ValidationError(SCHEMA_BASE, "closure_start_date", "daBeforeToday") // This date is in the future, please correct //TODO: look at 2 x msgs
+        ValidationError(SCHEMA_BASE, "closure_start_date", "daBeforeToday"), // This date is in the future, please correct //TODO: look at 2 x msgs
+        ValidationError(SCHEMA_BASE, "closure_start_date", "matchEndDateOrDateLastModified") // Match end_date or date last modified
       )
     }
 
@@ -80,7 +125,8 @@ class ClosureStartDateSpec extends AnyWordSpecLike {
     "error(s) if the value is invalid (in future)" in {
       val closedTestFileRow = closedMetadataFileRow(closureStartDate = Some("3024-12-25"))
       validationErrors(closedTestFileRow) should contain theSameElementsAs List(
-        ValidationError(SCHEMA_BASE, "closure_start_date", "daBeforeToday") // This date is in the future, please correct
+        ValidationError(SCHEMA_BASE, "closure_start_date", "daBeforeToday"), // This date is in the future, please correct
+        ValidationError(SCHEMA_BASE, "closure_start_date", "matchEndDateOrDateLastModified")
       )
     }
 
