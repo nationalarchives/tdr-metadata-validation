b93c2a44a356cbc932327685bdcb206421e88f3f
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-10-30 15:03:49 +0000

    review changes

--- src/test/scala/uk/gov/nationalarchives/tdr/validation/schema/fields/EvidenceProvidedBySpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/validation/schema/fields/EvidenceProvidedBySpec.scala
@@ -8,29 +8,29 @@ import uk.gov.nationalarchives.tdr.validation.schema.helpers.TestHelper._
 
 class EvidenceProvidedBySpec extends AnyWordSpecLike {
 
-  "When validating against schema we see" should {
+  "When validating against schema" should {
 
     "success if a valid former reference is provided" in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = Some("evidence provided by is added"))
       validationErrors(openTestFileRow).size shouldBe 0
     }
 
-    "succeeds if the cell itself is empty" in {
+    "success if the cell itself is empty" in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = Some(""))
       validationErrors(openTestFileRow).size shouldBe 0
     }
 
-    "succeeds if whole column is missing" in {
+    "success if whole column is missing" in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = None)
       validationErrors(openTestFileRow).size shouldBe 0
     }
 
-    "succeeds if a single char evidence provided by is provided " in {
+    "success if a single char evidence provided by is provided " in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = Some("a"))
       validationErrors(openTestFileRow).size shouldBe 0
     }
 
-    "succeeds if a 255 char evidence provided by is provided " in {
+    "success if a 255 char evidence provided by is provided " in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = Some(twoHundredAndFiftyFiveCharString))
       validationErrors(openTestFileRow).size shouldBe 0
     }
@@ -38,10 +38,11 @@ class EvidenceProvidedBySpec extends AnyWordSpecLike {
     "errors if a long evidence provided by is provided (this tests 256 char)" in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = Some(twoHundredAndFiftyFiveCharString + "1"))
       validationErrors(openTestFileRow) should contain theSameElementsAs List(
-        ValidationError(SCHEMA_BASE, "evidence_provided_by", "maxLength") // More than 255 characters, this field has a maximum length of 255 characters
+        ValidationError(SCHEMA_BASE, "evidence_provided_by", "maxLength")
       )
     }
-    "errors if evidence provided by contains \n" in {
+
+    "errors if evidence provided by contains \n (line break) character" in {
       val openTestFileRow = openMetadataFileRow(evidenceProvidedBy = Some("evidence\nprovided by"))
       validationErrors(openTestFileRow) should contain theSameElementsAs List(
         ValidationError(SCHEMA_BASE, "evidence_provided_by", "pattern")
