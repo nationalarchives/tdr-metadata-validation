a0454e08a57a2f247a5a61f3a23afbdea7fb9e8e
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2026-01-30 15:36:22 +0000

    allow use of environment specific schema

--- src/main/scala/uk/gov/nationalarchives/tdr/validation/schema/JsonSchemaDefinition.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/validation/schema/JsonSchemaDefinition.scala
@@ -1,20 +1,21 @@
 package uk.gov.nationalarchives.tdr.validation.schema
 
+import uk.gov.nationalarchives.tdr.schemautils.ConfigUtils
 import uk.gov.nationalarchives.tdr.validation.schema.ValidationProcess.{
   SCHEMA_BASE,
   SCHEMA_CLOSURE_CLOSED,
   SCHEMA_CLOSURE_OPEN,
-  SCHEMA_REQUIRED,
   SCHEMA_RELATIONSHIP,
+  SCHEMA_REQUIRED,
   ValidationProcess
 }
 
 sealed abstract class JsonSchemaDefinition(val schemaLocation: String, val validationProcess: ValidationProcess)
 
 object JsonSchemaDefinition {
-  case object BASE_SCHEMA extends JsonSchemaDefinition("/metadata-schema/baseSchema.schema.json", SCHEMA_BASE)
-  case object CLOSURE_SCHEMA_CLOSED extends JsonSchemaDefinition("/metadata-schema/closureSchemaClosed.schema.json", SCHEMA_CLOSURE_CLOSED)
-  case object CLOSURE_SCHEMA_OPEN extends JsonSchemaDefinition("/metadata-schema/closureSchemaOpen.schema.json", SCHEMA_CLOSURE_OPEN)
-  case object REQUIRED_SCHEMA extends JsonSchemaDefinition("/metadata-schema/requiredSchema.schema.json", SCHEMA_REQUIRED)
-  case object RELATIONSHIP_SCHEMA extends JsonSchemaDefinition("/metadata-schema/relationshipSchema.schema.json", SCHEMA_RELATIONSHIP)
+  case object BASE_SCHEMA extends JsonSchemaDefinition(ConfigUtils.mapToEnvironmentFile("/metadata-schema/baseSchema.schema.json"), SCHEMA_BASE)
+  case object CLOSURE_SCHEMA_CLOSED extends JsonSchemaDefinition(ConfigUtils.mapToEnvironmentFile("/metadata-schema/closureSchemaClosed.schema.json"), SCHEMA_CLOSURE_CLOSED)
+  case object CLOSURE_SCHEMA_OPEN extends JsonSchemaDefinition(ConfigUtils.mapToEnvironmentFile("/metadata-schema/closureSchemaOpen.schema.json"), SCHEMA_CLOSURE_OPEN)
+  case object REQUIRED_SCHEMA extends JsonSchemaDefinition(ConfigUtils.mapToEnvironmentFile("/metadata-schema/requiredSchema.schema.json"), SCHEMA_REQUIRED)
+  case object RELATIONSHIP_SCHEMA extends JsonSchemaDefinition(ConfigUtils.mapToEnvironmentFile("/metadata-schema/relationshipSchema.schema.json"), SCHEMA_RELATIONSHIP)
 }
