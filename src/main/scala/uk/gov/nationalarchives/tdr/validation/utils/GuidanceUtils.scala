fa1fdb72026b10fc54ebec4b0d2027bea538f975
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2026-02-11 09:53:41 +0000

    use schema fab

--- src/main/scala/uk/gov/nationalarchives/tdr/validation/utils/GuidanceUtils.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/validation/utils/GuidanceUtils.scala
@@ -5,12 +5,13 @@ import io.circe.{Decoder, DecodingFailure, HCursor}
 import io.circe.generic.auto._
 import io.circe.generic.semiauto.deriveDecoder
 import io.circe.jawn.decode
+import uk.gov.nationalarchives.tdr.schemautils.ConfigUtils
 
 import scala.io.Source
 import scala.util.Using
 
 object GuidanceUtils {
-  private val METADATA_GUIDANCE_LOCATION = "/guidance/metadata-template.json"
+  private val METADATA_GUIDANCE_LOCATION = ConfigUtils.mapToMetadataEnvironmentFile("/guidance/metadata-template.json")
 
   implicit val decodeExample: Decoder[String] = new Decoder[String] {
     final def apply(c: HCursor): Decoder.Result[String] = {
